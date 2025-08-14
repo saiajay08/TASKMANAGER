0f14c226c1dae803052cc57454cf03185eaf4c86
saiajay08 <pavansaitalla81@gmail.com>
2025-08-14 22:30:28 +0530

    Updated task manager

---
+++ app.js
@@ -0,0 +1,68 @@
+//variables
+const err=document.querySelector(".err");
+const inputTask=document.getElementById("input-Task");
+const addTaskbtn=document.getElementById("add-task");
+const inputsearch=document.getElementById("search-input");
+const taskList=document.querySelector(".task-list");
+const clearAllBtn=document.querySelector(".clear-all");
+//add a task
+//? click on add task
+addTaskbtn.addEventListener("click", (e) => {
+    //? check input task is not empty
+    if (inputTask.value !==''){
+        //? input value and trim
+        e.preventDefault();
+        const taskText = inputTask.value.trim();
+        //? create a new li
+        const newLi = document.createElement("li");
+        newLi.className= "task";
+        //? create an input field
+        const taskInput=document.createElement("input");
+        taskInput.type="text";
+        taskInput.disabled=true;
+        taskInput.className="disabled-task";
+        //? put the input trimmed value into the disabled task
+        taskInput.value=taskText;
+        //? put the input filled task into the new list
+        newLi.appendChild(taskInput);
+        //? create delete button
+        const deleteBtn=document.createElement("button");
+        deleteBtn.innerText="Delete";
+        deleteBtn.className="deleteBtn";
+        //? put the delete button in li
+        newLi.appendChild(deleteBtn);
+        //? create edit button
+        const editBtn=document.createElement("button");
+        editBtn.innerText="Edit";
+        editBtn.className="editBtn";
+        //? put the edit button in li
+        newLi.appendChild(editBtn);
+        //? put new li that contain all tasks
+        taskList.append(newLi);
+        //? clear the input task
+        inputTask.value="";
+
+    }
+    else{
+        err.style.display="block";
+        setTimeout(()=>{
+            err.style.display="none";
+        },2000);
+    }
+
+});
+
+    
+
+
+
+
+
+
+//edit a task
+
+//delete a task
+
+//delete all tasks
+
+//search a task
